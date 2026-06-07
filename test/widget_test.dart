b6f0669f0596055a93f7389105a9d903cc1a1bc2
Félix Bernatchez <felix.bernatchez@hotmail.com>
2026-06-06 23:16:34 -0400

    Bump version to 1.0.1-beta.2+17 and update version assertions in widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -40,7 +40,7 @@ class MockDragyProvider extends ChangeNotifier implements DragyProvider {
   double? longitude;
 
   @override
-  String appVersion = '1.0.1-beta.1';
+  String appVersion = '1.0.1-beta.2';
 
   @override
   BluetoothDevice? connectedDevice;
@@ -554,7 +554,7 @@ void main() {
     expect(mockProvider.tempInCelsius, false);
 
     // Verify version info is displayed
-    expect(find.text('1.0.1-beta.1'), findsOneWidget);
+    expect(find.text('1.0.1-beta.2'), findsOneWidget);
   });
 
   testWidgets('GarageScreen displays empty state and lists vehicles', (WidgetTester tester) async {
