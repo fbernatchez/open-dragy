bc653be474167d91afc79a41829e741e14fdeb14
Félix Bernatchez <felix.bernatchez@hotmail.com>
2026-06-22 07:40:17 -0400

    chore: rename package to com.fb_engineering

---
+++ android/app/src/main/kotlin/com/fb_engineering/open_dragy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fb_engineering.open_dragy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/fbengineering/open_dragy/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.fbengineering.open_dragy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
